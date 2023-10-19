0605dda61830a32935dab2f459c9a4051c7fcc31
vikas sharma <134483699+vikaas-sharma@users.noreply.github.com>
2023-10-19 22:23:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 // STATIC ROUTES
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build"));
 });
 
 //port
